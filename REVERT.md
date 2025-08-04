54262b01b0121892a84058075796f332092140ac
Shahab Matapour <sh.matapour@gmail.com>
2025-08-04 10:25:55 -0400

    Controlled and Uncontrolled components

--- REVERT.md
+++
@@ -1,3 +0,0 @@
-This commit reverts the merge commit cb652924e2f35849782736b833d6a6dabe5e2c18 (the Container merge).
-
-Revert: cb652924e2f35849782736b833d6a6dabe5e2c18
--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,56 @@
-import axios from 'axios'
-import { CurrentUserLoader } from "./CurrentUserLoader"
-import { DataLoader } from "./DataLoader"
-import { ResourceLoader } from "./ResourceLoader"
-import { UserInfo } from "./UserInfo"
-import { UserLoader } from "./UserLoader"
+import { useState } from 'react';
+import { ControlledOnboardingFlow } from './ControlledOnboardingFlow';
+import { UncontrolledOnboardingFlow } from './UncontrolledOnboardingFlow';
 
-const getServerData = url => async () => {
-  const response = await axios.get(url);
-  return response.data
-}
+const StepOne = ({ goToNext }) => (
+	<>
+	<h1>Step 1</h1>
+	<button onClick={() => goToNext({ name: 'John Doe' })}>Next</button>
+	</>
+);
 
-const getTestData = () => {
-  return {
-    id: '12345',
-    name: 'Blah blah',
-    age: 54,
-    hairColor: 'brown',
-    hobbies: ['swimming', 'bicycling', 'video games'],
-  }
-}
+const StepTwo = ({ goToNext }) => (
+	<>
+	<h1>Step 2</h1>
+	<button onClick={() => goToNext({ age: 40 })}>Next</button>
+	</>
+);
+
+const StepThree = ({ goToNext }) => (
+	<>
+	<h1>Congratulations! You qualify for the senior discount!</h1>
+	<button onClick={() => goToNext({ hairColor: 'brown' })}>Next</button>
+	</>
+);
+
+const StepFour = ({ goToNext }) => (
+	<>
+	<h1>Step 4</h1>
+	<button onClick={() => goToNext({ hairColor: 'brown' })}>Next</button>
+	</>
+);
 
 function App() {
-  return (
-    <>
-    <DataLoader
-      getDataFn={getTestData}
-      resourceName="user"
+  const [onboardingData, setOnboardingData] = useState({});
+	const [currentIndex, setCurrentIndex] = useState(0);
+
+  const onNext = stepData => {
+    setOnboardingData({ ...onboardingData, ...stepData });
+    setCurrentIndex(currentIndex + 1);
+  }
+
+	return (
+		<ControlledOnboardingFlow
+      currentIndex={currentIndex}
+      onNext={onNext}
+      onFinish={() => alert('Done!')}
     >
-      <UserInfo />
-    </DataLoader>
-    <ResourceLoader
-      resourceUrl="/api/users/345"
-      resourceName="user"
-    >
-      <UserInfo />
-    </ResourceLoader>
-    <UserLoader userId="345">
-      <UserInfo />
-    </UserLoader>
-    </>
-  )
+			<StepOne />
+			<StepTwo />
+			{onboardingData.age >= 65 && <StepThree />}
+      <StepFour />
+		</ControlledOnboardingFlow>
+	);
 }
 
-export default App
+export default App;
