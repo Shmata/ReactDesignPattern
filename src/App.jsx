2f2d14ed9ab3dc24b19d7c29ac308bf7cd60f742
Shahab Matapour <sh.matapour@gmail.com>
2025-08-07 21:12:43 -0400

    Function components

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,12 @@
-import axios from 'axios'
-import { CurrentUserLoader } from "./CurrentUserLoader"
-import { DataLoader } from "./DataLoader"
-import { ResourceLoader } from "./ResourceLoader"
-import { UserInfo } from "./UserInfo"
-import { UserLoader } from "./UserLoader"
+import { DangerButton, SuccessButton } from "./partialApplication"
 
-const getServerData = url => async () => {
-  const response = await axios.get(url);
-  return response.data
-}
-
-const getTestData = () => {
-  return {
-    id: '12345',
-    name: 'Blah blah',
-    age: 54,
-    hairColor: 'brown',
-    hobbies: ['swimming', 'bicycling', 'video games'],
-  }
-}
 
 function App() {
   return (
-    <>
-    <DataLoader
-      getDataFn={getTestData}
-      resourceName="user"
-    >
-      <UserInfo />
-    </DataLoader>
-    <ResourceLoader
-      resourceUrl="/api/users/345"
-      resourceName="user"
-    >
-      <UserInfo />
-    </ResourceLoader>
-    <UserLoader userId="345">
-      <UserInfo />
-    </UserLoader>
-    </>
+		<>
+		<DangerButton text="Self Destruct!" />
+		<SuccessButton size="large" />
+		</>
   )
 }
 
